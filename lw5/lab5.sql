c6ab3a313e21a46baf7660de09fa707c0e69b6dc
igormayshanov <79751645+igormayshanov@users.noreply.github.com>
2022-05-01 18:07:16 +0500

    add tasks 5, 8, 9

--- lw5/lab5.sql
+++ lw5/lab5.sql
@@ -14,22 +14,23 @@ ALTER TABLE hotel_complex.dbo.room
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 ;
+
 ALTER TABLE hotel_complex.dbo.room
-	ADD CONSTRAINT FK_room_room_category FOREIGN KEY (id_room_category)
+	ADD CONSTRAINT FK_room_id_room_category FOREIGN KEY (id_room_category)
 		REFERENCES hotel_complex.[dbo].[room_category] (id_room_category)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 ;
 
 ALTER TABLE hotel_complex.[dbo].[room_in_booking]
-	ADD CONSTRAINT FK_room_in_booking_booking FOREIGN KEY (id_booking)
+	ADD CONSTRAINT FK_room_in_booking_id_booking FOREIGN KEY (id_booking)
 		REFERENCES hotel_complex.[dbo].[booking] (id_booking)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 ;
 
 ALTER TABLE hotel_complex.[dbo].[room_in_booking]
-	ADD CONSTRAINT FK_room_in_booking_room FOREIGN KEY (id_room)
+	ADD CONSTRAINT FK_room_in_booking_id_room FOREIGN KEY (id_room)
 		REFERENCES hotel_complex.[dbo].[room] (id_room)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
@@ -46,7 +47,6 @@ JOIN [dbo].room ON room.id_room = room_in_booking.id_room
 JOIN [dbo].room_category ON room_category.id_room_category = room.id_room_category
 JOIN [dbo].[hotel] ON hotel.id_hotel = room.id_hotel
 WHERE hotel.name = 'Космос' AND room_category.name = 'Люкс' AND checkin_date <= '2019.04.01' AND '2019.04.01' < checkout_date;
-
 
 SELECT *
 FROM [dbo].[client]
@@ -96,7 +96,7 @@ WHERE room.id_room NOT IN (
 		FROM [dbo].[room_in_booking]
 		WHERE checkin_date <= '2019.04.22' AND '2019.04.22' < checkout_date);
 
-SELECT * FROM [dbo].[room]
+SELECT room.number, hotel.name FROM [dbo].[room]
 LEFT JOIN [dbo].hotel ON room.id_hotel = hotel.id_hotel
 WHERE room.id_room NOT IN(SELECT id_room FROM [dbo].[room_in_booking]
 						WHERE checkin_date <= '2019.04.22' AND '2019.04.22' < checkout_date)
@@ -118,6 +118,7 @@ GROUP BY room.id_room_category;
 
 -- 5. Дать список последних проживавших клиентов по всем комнатам гостиницы
 -- “Космос”, выехавшим в апреле с указанием даты выезда. 
+-- TODO комната, человек, дата
 
 SELECT client.name, MAX(room_in_booking.checkout_date) AS max_checkout_date FROM [dbo].[client]
 JOIN [dbo].[booking] ON client.id_client = booking.id_client
@@ -126,8 +127,40 @@ JOIN [dbo].[room] ON room_in_booking.id_room = room.id_room
 JOIN [dbo].[hotel] ON room.id_hotel = hotel.id_hotel AND hotel.name = 'Космос'
 GROUP BY client.name;
 
+SELECT room.number, client.name, room_in_booking.checkout_date FROM [dbo].[client]
+JOIN [dbo].[booking] ON client.id_client = booking.id_client
+JOIN [dbo].room_in_booking ON booking.id_booking = room_in_booking.id_booking AND room_in_booking.checkout_date BETWEEN '2019.04.01' AND '2019.04.30'
+JOIN [dbo].[room] ON room_in_booking.id_room = room.id_room
+JOIN [dbo].[hotel] ON room.id_hotel = hotel.id_hotel AND hotel.name = 'Космос';
+--GROUP BY client.name;
+
+SELECT room.number, MAX(client.name) AS client_name, MAX(room_in_booking.checkout_date) AS max_checkout_date FROM [dbo].[room]
+JOIN [dbo].[room_in_booking] ON room.id_room = room_in_booking.id_room AND room_in_booking.checkout_date BETWEEN '2019.04.01' AND '2019.04.30'
+JOIN [dbo].[hotel] ON room.id_hotel = hotel.id_hotel AND hotel.name = 'Космос'
+JOIN [dbo].[booking] ON room_in_booking.id_booking = booking.id_booking
+JOIN [dbo].[client] ON client.id_client = booking.id_client
+GROUP BY room.number
+ ;
+
 -- 6. Продлить на 2 дня дату проживания в гостинице “Космос” всем клиентам
- -- комнат категории “Бизнес”, которые заселились 10 мая. UPDATE room_in_booking SET checkout_date = DATEADD(day, 2, checkout_date) WHERE id_room_in_booking IN (SELECT rib.id_room_in_booking FROM [dbo].[room_in_booking] rib								JOIN [dbo].[room] ON rib.id_room = room.id_room								 JOIN [dbo].[hotel] ON room.id_hotel = hotel.id_hotel AND hotel.name = 'Космос'								 JOIN [dbo].[room_category] ON room.id_room_category = room_category.id_room_category AND room_category.name = 'Бизнес'								 WHERE checkin_date = '2019.05.10');SELECT * FROM [dbo].[room_in_booking] ribJOIN [dbo].[room] ON rib.id_room = room.id_roomJOIN [dbo].[hotel] ON room.id_hotel = hotel.id_hotel AND hotel.name = 'Космос'JOIN [dbo].[room_category] ON room.id_room_category = room_category.id_room_category AND room_category.name = 'Бизнес'WHERE checkin_date = '2019.05.10';UPDATE room_in_booking SET checkin_date = '2019.05.10'WHERE id_room_in_booking IN (118, 1495, 928, 624);-- 7. Найти все "пересекающиеся" варианты проживания. Правильное состояние: не 
+ -- комнат категории “Бизнес”, которые заселились 10 мая.
+UPDATE room_in_booking SET checkout_date = DATEADD(day, 2, checkout_date)
+WHERE id_room_in_booking IN (SELECT rib.id_room_in_booking FROM [dbo].[room_in_booking] rib
+								JOIN [dbo].[room] ON rib.id_room = room.id_room
+								JOIN [dbo].[hotel] ON room.id_hotel = hotel.id_hotel AND hotel.name = 'Космос'
+								JOIN [dbo].[room_category] ON room.id_room_category = room_category.id_room_category AND room_category.name = 'Бизнес'
+								WHERE checkin_date = '2019.05.10');
+
+SELECT * FROM [dbo].[room_in_booking] rib
+JOIN [dbo].[room] ON rib.id_room = room.id_room
+JOIN [dbo].[hotel] ON room.id_hotel = hotel.id_hotel AND hotel.name = 'Космос'
+JOIN [dbo].[room_category] ON room.id_room_category = room_category.id_room_category AND room_category.name = 'Бизнес'
+WHERE checkin_date = '2019.05.10';
+
+UPDATE room_in_booking SET checkin_date = '2019.05.10'
+WHERE id_room_in_booking IN (118, 1495, 928, 624);
+
+-- 7. Найти все "пересекающиеся" варианты проживания. Правильное состояние: не 
 -- может быть забронирован один номер на одну дату несколько раз, т.к. нельзя
 -- заселиться нескольким клиентам в один номер. Записи в таблице
 -- room_in_booking с id_room_in_booking = 5 и 2154 являются примером
@@ -141,5 +174,53 @@ WHERE rib1.id_room = rib2.id_room AND rib1.id_room_in_booking != rib2.id_room_in
 
 -- 8. Создать бронирование в транзакции.
 BEGIN TRANSACTION
-	
-COMMIT
+	INSERT INTO [dbo].[client]
+	VALUES 
+		('Иванов Иван Иванович', '89623412345')
+	INSERT INTO [dbo].[booking]
+	VALUES
+		((SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]), '2022.05.01')
+	INSERT INTO [dbo].[room_in_booking]
+	VALUES
+		((SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]), 1, '2022.05.02', '2022.05.07')
+COMMIT;
+
+SELECT * FROM dbo.client
+WHERE name = 'Иванов Иван Иванович';
+
+SELECT client.name, client.phone, rib.id_room_in_booking, rib.id_booking, rib.checkin_date, rib.checkout_date FROM dbo.room_in_booking rib
+JOIN dbo.booking ON rib.id_booking = booking.id_booking
+JOIN dbo.client ON booking.id_client = client.id_client
+WHERE client.name = 'Иванов Иван Иванович';
+
+-- 9. Добавить необходимые индексы для всех таблиц
+
+CREATE NONCLUSTERED INDEX [IX_room_id_hotel] ON
+[dbo].[room]
+(
+	[id_hotel] ASC
+);
+
+CREATE NONCLUSTERED INDEX [IX_room _in_booking_id_booking] ON
+[dbo].[room_in_booking]
+(
+	[id_booking] ASC
+);
+
+CREATE NONCLUSTERED INDEX [IX_room _in_booking_id_room] ON
+[dbo].[room_in_booking]
+(
+	[id_room] ASC
+);
+
+CREATE NONCLUSTERED INDEX [IX_booking_id_client] ON
+[dbo].[booking]
+(
+	[id_client] ASC
+);
+
+CREATE NONCLUSTERED INDEX [IX_room_id_room_category] ON
+[dbo].[room]
+(
+	[id_room_category] ASC
+);
